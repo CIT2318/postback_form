087dea073e9aaebe5213a4f8867665147407d7c1
Matthew Mantle <memantle@googlemail.com>
2016-10-02 21:28:10 +0100

    updated instructions

--- postback.php
+++ postback.php
@@ -1,3 +1,21 @@
+<?php 
+$msg="";
+$userAns="";
+if(isset($_POST["answerBtn"]))
+{
+    $userAns=$_POST["capital"];
+    if($userAns===""){
+        $msg = "You need to answer the question";
+    }else{
+        if($userAns==="Paris"){
+        	$msg = "You are correct";
+    	}else{
+    		$msg = "You are wrong";
+    	}
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,11 +23,14 @@
 </head>
 <body>
 
-<form action="process.php" method="POST">
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
 <label for="capital">What is the capital of France:</label>
-<input type="text" name="capital">
+<input type="text" value="<?php echo $userAns;?>" name="capital">
 <input type="submit" name="answerBtn">
 </form>
 
+<?php
+echo "<p>".$msg."</p>";
+?>
 </body>
 </html>
